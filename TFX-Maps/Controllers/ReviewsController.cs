ba78420b0a682641e90e129b4b0e56cc866b2696
Jo <jo@greenmeadowsoftware.com>
2019-04-09 22:21:42 +0100

    Initial commit

---
+++ TFX-Maps/Controllers/ReviewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TFX_Maps.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Top10()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TFX-Maps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TFX_Maps.MvcApplication" Language="C#" %>
