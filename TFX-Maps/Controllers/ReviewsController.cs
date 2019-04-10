b9fdfd90b40550c1b3e5e7e79f65d9846fb0f696
Jo <jo@greenmeadowsoftware.com>
2019-04-10 22:06:05 +0100

    Added Top 10

--- TFX-Maps/Controllers/ReviewsController.cs
+++ TFX-Maps/Controllers/ReviewsController.cs
@@ -16,7 +16,37 @@ namespace TFX_Maps.Controllers
 
         public ActionResult Top10()
         {
+            var r = new Random();
+            ViewBag.Reviews = new[] { this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r), this.GenerateReview(r) }
+                .OrderByDescending(v => v.Score)
+                .ToArray();
             return View();
+        }
+
+        public ActionResult View(Guid id)
+        {
+            ViewBag.Review = this.GenerateReview(new Random());
+            return View();
+        }
+
+        private Models.ReviewModel GenerateReview(Random r)
+        {
+            return new Models.ReviewModel
+            {
+                Name = "Random Map",
+                Author = "Ted",
+                AuthorEmail = "ted@example.org",
+                Score = r.Next(100),
+                UserRating = r.NextDouble() * 5,
+                UserVotes = r.Next(10),
+                Architecture = r.Next(100),
+                Layout = r.Next(100),
+                Funness = r.Next(100),
+                Originality = r.Next(100),
+                Balance = r.Next(100),
+                Review = "This is a good map",
+                Reviewer = "Prof. Wimbleton"
+            };
         }
 
     }
---
+++ TFX-Maps/Models/ReviewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TFX_Maps.Models
+{
+    public class ReviewModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime ReviewDate { get; set; } = DateTime.Now;
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string AuthorEmail { get; set; } = null;
+        public Uri DownloadUrl { get; set; } = null;
+        public Uri HomepageUrl { get; set; } = null;
+        public int Score { get; set; }
+        public double UserRating { get; set; }
+        public int UserVotes { get; set; }
+        public int Architecture { get; set; }
+        public int Layout { get; set; }
+        public int Funness { get; set; }
+        public int Originality { get; set; }
+        public int Balance { get; set; }
+        public string Review { get; set; }
+        public string Reviewer { get; set; }
+
+    }
+}
